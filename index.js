837d9b9934bbeba6873f1360ae2df5bbb6d951b6
Brian <b.perez3237@gmail.com>
2022-02-12 08:48:02 -0500

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Code your solution here
+
+
+function findMatching(dArray, string) {
+    const result = dArray.filter(driver => driver.toUpperCase() === string.toUpperCase());
+    return result;
+}
+
+function fuzzyMatch(dArray, str) { 
+    const res = dArray.filter(function (driver) {
+
+        if (driver.slice(0,str.length) === str) {
+            return true;
+        }
+        return false;
+    });
+    return res;
+}
+
+function matchName(dArray, str) {
+
+    const res = dArray.filter(function (driver) {
+
+        if (driver.name === str) {
+            return true;
+        }
+        return false;
+    });
+    return res;
+}
+
+
